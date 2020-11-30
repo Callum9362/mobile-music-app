55c579030d70499ecdaaa046a51fd2cba74e9fec
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-01 09:55:17 +1100

    Added in final styling for the music player

--- src/App.vue
+++ src/App.vue
@@ -3,81 +3,95 @@
     <header>
       <h1>My Music</h1>
     </header>
-      <main>
-        <section class="player">
-          <h2 class="song-title">{{ current.title }} - <span>{{current.artist}}</span></h2>
-          <div class="controls">
-            <button class="prev" @click="prev">Prev</button>
-            <button class="play" v-if="!isPlaying" @click="play">Play</button>
-            <button class="pause" v-else @click="pause">Pause</button>
-            <button class="next" @click="next">Next</button>
-          </div>
-        </section>
-        <section class="playlist">
-          <h3>Playlist</h3>
-          <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
-             {{ song.title }} - {{ song.artist }}
-          </button>
-        </section>
-      </main>
+    <main>
+      <section class="player">
+        <h2 class="song-title">
+          {{ current.title }} - <span>{{ current.artist }}</span>
+        </h2>
+        <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>Playlist</h3>
+        <button
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(song)"
+          :class="song.src == current.src ? 'song playing' : 'song'"
+        >
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  
-  name: 'App',
-  
-  data () {
+  name: "App",
+
+  data() {
     return {
       current: {},
       index: 0,
       isPlaying: false,
       songs: [
         {
-          title: 'A new beginning',
-          artist: 'Ben Sound',
-          src: require('./assets/bensound-anewbeginning.mp3')
+          title: "A new beginning",
+          artist: "Ben Sound",
+          src: require("./assets/bensound-anewbeginning.mp3"),
         },
         {
-          title: 'Creative Minds',
-          artist: 'Ben Sound',
-          src: require('./assets/bensound-creativeminds.mp3')
+          title: "Creative Minds",
+          artist: "Ben Sound",
+          src: require("./assets/bensound-creativeminds.mp3"),
         },
         {
-          title: 'Ukulele',
-          artist: 'Ben Sound',
-          src: require('./assets/bensound-ukulele.mp3')
-        }
+          title: "Ukulele",
+          artist: "Ben Sound",
+          src: require("./assets/bensound-ukulele.mp3"),
+        },
       ],
-      player: new Audio()
-    }
+      player: new Audio(),
+    };
   },
 
   methods: {
-
-    play (song) {
-
-      if(typeof song.src != "undefined"){
+    play(song) {
+      if (typeof song.src != "undefined") {
         this.current = song;
         this.player.src = this.current.src;
       }
       this.player.play();
+      this.player.addEventListener(
+        "ended",
+        function () {
+          this.index++;
+
+          if (this.index > this.songs.length - 1) {
+            this.index = 0;
+          }
+
+          this.current = this.songs[this.index];
+          this.play(this.current);
+        }.bind(this)
+      );
       this.isPlaying = true;
     },
 
-    pause () {
+    pause() {
       this.player.pause();
       this.isPlaying = false;
     },
 
-    next () {
-
+    next() {
       this.index++;
-      
-      if(this.index > this.songs.length -1){
+
+      if (this.index > this.songs.length - 1) {
         this.index = 0;
       }
 
@@ -85,27 +99,22 @@ export default {
       this.play(this.current);
     },
 
-    prev () {
+    prev() {
       this.index--;
-      if(this.index < 0){
+      if (this.index < 0) {
         this.index = this.songs.length - 1;
       }
       this.current = this.songs[this.index];
       this.play(this.current);
-    }
-
+    },
   },
 
-  created(){
+  created() {
     this.current = this.songs[this.index];
     this.player.src = this.current.src;
     //this.player.play();
-  }
-
-
-}
-
-
+  },
+};
 </script>
 
 <style>
@@ -127,6 +136,96 @@ header {
   padding: 15px;
   background-color: #212121;
   color: white;
+}
+
+main {
+  width: 100%;
+  max-width: 760px;
+  margin: 0 auto;
+  padding: 25px;
+}
+
+.song-title {
+  color: #53565A;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+
+button{
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: none;
+}
+
+button:hover{
+  opacity: 0.8;
+}
+
+.play, .pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: white;
+  background-color: #CC2E5D;
+}
+
+.next, .prev{
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: white;
+  background-color: #FF5858;
+}
+
+.playlist {
+  padding: 0px 30px;
+
+}
+
+.playlist h3{
+  color: black;
+  font-size: 20px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.playlist .song:hover{
+  color: #FF5858;
+}
+
+.playlist .song.playing {
+  color: white;
+  background-image: linear-gradient(to right, #CC2E5D, #FF5858)
 }
 
 </style>
