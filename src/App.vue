e75cf201f7f14444be93b6f78257a3ea30292bea
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-01 08:17:28 +1100

    Added the basic mp3 files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <h1>Testing my app</h1>
+    <header>
+      <h1>My Music</h1>
+    </header>
+      <main>
+        <section class="player">
+          <h2 class="song-title">{{ current.title }} - <span>{{current.artist}}</span></h2>
+        </section>
+      </main>
   </div>
 </template>
 
@@ -8,17 +15,56 @@
 
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      current: {
+        title: 'SONG TITLE'
+      },
+      songs: [
+        {
+          title: 'A new beginning',
+          artist: 'Ben Sound',
+          src: require('./assets/bensound-anewbeginning.mp3')
+        },
+        {
+          title: 'Creative Minds',
+          artist: 'Ben Sound',
+          src: require('./assets/bensound-creativemings.mp3')
+        },
+        {
+          title: 'Ukulele',
+          artist: 'Ben Sound',
+          src: require('./assets/bensound-ukulele.mp3')
+        }
+      ]
+
+    }
+  }
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+body {
+  font-family: sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: white;
+}
+
 </style>
